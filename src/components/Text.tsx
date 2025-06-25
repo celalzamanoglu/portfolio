e124530ee6e14ccaef229e371131d4b79c6472d5
Celal Zamanoglu <celal.zamanoglu@hotmail.com>
2025-06-26 01:37:44 +0300

    feat: temporary scene and text added

---
+++ src/components/Text.tsx
@@ -0,0 +1,10 @@
+export default function Text() {
+  return (
+    <div className="absolute flex flex-col text-[5.5vw] uppercase w-[80vw] items-start leading-tight">
+      <p>Celal Zamanoğlu</p>
+      <p className="self-end">Full Stack Developer</p>
+      <p>Based in Istanbul</p>
+      <p className="self-end">Portfolio@2025</p>
+    </div>
+  );
+}
