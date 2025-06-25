75fe85a8b515320ba279aa089ad8fb77b8d76d23
Celal Zamanoglu <celal.zamanoglu@hotmail.com>
2025-06-26 01:55:33 +0300

    feat: she is now too happy so mission accomplished - joke is over

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,22 +1,11 @@
-import Image from "next/image";
-
 export default function Text() {
   return (
     <>
       <div className="absolute flex flex-col text-[5.5vw] uppercase w-[80vw] items-start leading-tight">
-        <p>Aşkım</p>
-        <p className="self-end">Benimle</p>
-        <p>Evlenir</p>
-      </div>
-
-      {/* Love text in bottom left */}
-      <div className="absolute bottom-4 left-4 text-sm text-black">
-        seni seviyorum aşkım hayatım her şeyim güzelliğim
-      </div>
-
-      {/* Image in bottom right */}
-      <div className="absolute bottom-20 right-80">
-        <Image src="/recep-nah.jpg" alt="Recep Nah" width={150} height={150} />
+        <p>Celal Zamanoglu</p>
+        <p className="self-end">Full Stack Developer</p>
+        <p>Based in Istanbul</p>
+        <p className="self-end">Portfolio@2025</p>
       </div>
     </>
   );
